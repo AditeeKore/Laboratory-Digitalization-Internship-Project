0a0d920851f08d88ba86b3bd9f6fa7c24095ac1c
AditeeKore <aditeembk@gmail.com>
2021-07-14 12:36:02 +0530

    dept pages

--- LaboratoryDigitalization/Home/urls.py
+++ LaboratoryDigitalization/Home/urls.py
@@ -3,6 +3,15 @@ from django.urls import path
 from Home import views
 
 urlpatterns = [
-    path("", views.index, name='home'),
+    path("", views.index, name='index'),
+    path("index", views.index, name='index'),
     path("about", views.about, name='about'),
+    path("firstyear", views.firstyear, name='firstyear'),
+    path("inft", views.inft, name='inft'),
+    path("cmpn", views.cmpn, name='cmpn'),
+    path("extc", views.extc, name='extc'),
+    path("etrx", views.etrx, name='etrx'),
+    path("biomed", views.biomed, name='biomed'),
+
+
 ]
--- LaboratoryDigitalization/Home/views.py
+++ LaboratoryDigitalization/Home/views.py
@@ -8,3 +8,23 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
+def firstyear(request):
+    return render(request, 'firstyear.html')
+
+def inft(request):
+    return render(request, 'inft.html')
+
+def cmpn(request):
+    return render(request, 'cmpn.html')
+
+def extc(request):
+    return render(request, 'extc.html')
+
+def etrx(request):
+    return render(request, 'etrx.html')
+
+def biomed(request):
+    return render(request, 'biomeds.html')
+
+
+
