1b3cb172b7466a26923829ed943df78d1234d496
AditeeKore <aditeembk@gmail.com>
2021-07-18 15:02:01 +0530

    changes in pages

--- LaboratoryDigitalization/Home/views.py
+++ LaboratoryDigitalization/Home/views.py
@@ -13,18 +13,48 @@ def firstyear(request):
 
 def inft(request):
     return render(request, 'inft.html')
+def inftse(request):
+    return render(request, 'inftse.html')
+def inftte(request):
+    return render(request, 'inftte.html')
+def inftbe(request):
+    return render(request, 'inftbe.html')
 
 def cmpn(request):
     return render(request, 'cmpn.html')
+def cmpnse(request):
+    return render(request, 'cmpnse.html')
+def cmpnte(request):
+    return render(request, 'cmpnte.html')
+def cmpnbe(request):
+    return render(request, 'cmpnbe.html')
 
 def extc(request):
     return render(request, 'extc.html')
+def extcse(request):
+    return render(request, 'extcse.html')
+def extcte(request):
+    return render(request, 'extcte.html')
+def extcbe(request):
+    return render(request, 'extcbe.html')
 
 def etrx(request):
     return render(request, 'etrx.html')
+def etrxse(request):
+    return render(request, 'etrxse.html')
+def etrxte(request):
+    return render(request, 'etrxte.html')
+def etrxbe(request):
+    return render(request, 'etrxbe.html')
 
 def biomed(request):
-    return render(request, 'biomeds.html')
+    return render(request, 'biomed.html')
+def biomedse(request):
+    return render(request, 'biomedse.html')
+def biomedte(request):
+    return render(request, 'biomedte.html')
+def biomedbe(request):
+    return render(request, 'biomedbe.html')
 
 def bee(request):
     return render(request, 'bee.html')
