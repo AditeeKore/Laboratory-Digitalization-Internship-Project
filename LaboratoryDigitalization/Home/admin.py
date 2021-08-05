687ba862bc8a9f35d6ad4cb9b0cbf27d679905c5
AditeeKore <aditeembk@gmail.com>
2021-08-05 20:09:51 +0530

    uploaded lab data through excel files into database

--- LaboratoryDigitalization/Home/admin.py
+++ LaboratoryDigitalization/Home/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import AllLabinfo, Lab7A, Lab7A_SW_Inst, Lab7B_SW_Inst, Lab7C_SW_Inst, Lab7D_SW_Inst, Lab7E_SW_Inst, Lab7B, Lab7C, Lab7D, Lab7E
 
 # Register your models here.
+
+@admin.register(AllLabinfo)
+class uploadadmin(ImportExportModelAdmin):
+    list_display = ('Lab', 'NumberofPCs', 'BasicConfig', 'PCspurchasedinYr', 'OS', 'SpecialSoftware')
+
+@admin.register(Lab7A)
+class Lab7Ainfo(ImportExportModelAdmin):
+    list_display = ('ComputerName', 'Processor', 'RAM', 'HardDisk', 'MotherBoard', 'Basic_OS', 'SpecialPurpose', 'DateOfPurchase', 'WorkingStatus')
+
+@admin.register(Lab7B)
+class Lab7Binfo(ImportExportModelAdmin):
+    list_display = ('ComputerName', 'Processor', 'RAM', 'HardDisk', 'MotherBoard', 'Basic_OS', 'SpecialPurpose', 'DateOfPurchase', 'WorkingStatus')
+
+@admin.register(Lab7C)
+class Lab7Cinfo(ImportExportModelAdmin):
+    list_display = ('ComputerName', 'Processor', 'RAM', 'HardDisk', 'MotherBoard', 'Basic_OS', 'SpecialPurpose', 'DateOfPurchase', 'WorkingStatus')
+
+@admin.register(Lab7D)
+class Lab7Dinfo(ImportExportModelAdmin):
+    list_display = ('ComputerName', 'Processor', 'RAM', 'HardDisk', 'MotherBoard', 'Basic_OS', 'SpecialPurpose', 'DateOfPurchase', 'WorkingStatus')
+
+@admin.register(Lab7E)
+class Lab7Einfo(ImportExportModelAdmin):
+    list_display = ('ComputerName', 'Processor', 'RAM', 'HardDisk', 'MotherBoard', 'Basic_OS', 'SpecialPurpose', 'DateOfPurchase', 'WorkingStatus')
+
+@admin.register(Lab7A_SW_Inst)
+class Lab7Aswinfo(ImportExportModelAdmin):
+    list_display = ('SoftwareInstalled', 'Version')
+
+@admin.register(Lab7B_SW_Inst)
+class Lab7Bswinfo(ImportExportModelAdmin):
+    list_display = ('SoftwareInstalled', 'Version')
+
+@admin.register(Lab7C_SW_Inst)
+class Lab7Cswinfo(ImportExportModelAdmin):
+    list_display = ('SoftwareInstalled', 'Version')
+
+@admin.register(Lab7D_SW_Inst)
+class Lab7Dswinfo(ImportExportModelAdmin):
+    list_display = ('SoftwareInstalled', 'Version')
+
+@admin.register(Lab7E_SW_Inst)
+class Lab7Eswinfo(ImportExportModelAdmin):
+    list_display = ('SoftwareInstalled', 'Version')
--- LaboratoryDigitalization/Home/models.py
+++ LaboratoryDigitalization/Home/models.py
@@ -1,3 +1,93 @@
+from typing import Tuple
 from django.db import models
 
 # Create your models here.
+
+class AllLabinfo(models.Model):
+    Lab = models.CharField(max_length=20, unique=True, )
+    NumberofPCs = models.IntegerField()
+    BasicConfig = models.CharField(max_length=150, null = True)
+    PCspurchasedinYr =models.CharField(max_length=30, null = True)
+    OS = models.CharField(max_length=75, null = True)
+    SpecialSoftware=models.CharField(max_length=100, null = True)
+    # def __str__(self):
+    #     return self.cod+" "+self.nume
+    # class Meta:
+    #     ordering = ["cod"]
+    #     verbose_name_plural = "uploads"
+
+class Lab7A(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+
+class Lab7B(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab7C(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab7D(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab7E(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab7A_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab7B_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab7C_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab7D_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab7E_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
