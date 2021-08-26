6b09a2b08248d0d0d181dbef88edc4f9b678268d
AditeeKore <aditeembk@gmail.com>
2021-08-26 14:09:17 +0530

    displaying all excel files from lab7
    Facing difficulty in displaying lab7aswinfo and lab7bswinfo

--- LaboratoryDigitalization/Home/urls.py
+++ LaboratoryDigitalization/Home/urls.py
@@ -9,9 +9,15 @@ urlpatterns = [
     path("lab7", views.lab7, name='lab7'),
     path("lab7a", views.lab7Ainfo, name='lab7a'),
     path("lab7b", views.lab7Binfo, name='lab7b'),
+    path("lab7c", views.lab7Cinfo, name='lab7c'),
+    path("lab7d", views.lab7Dinfo, name='lab7d'),
+    path("lab7e", views.lab7Einfo, name='lab7e'),
     path("Upload", views.upload, name='upload'),
     path('lab7aswinfo', views.lab7aswinfo, name='lab7aswinfo'),
     path('lab7bswinfo', views.lab7bswinfo, name='lab7bswinfo'),
+    path('lab7cswinfo', views.lab7cswinfo, name='lab7cswinfo'),
+    path('lab7dswinfo', views.lab7dswinfo, name='lab7dswinfo'),
+    path('lab7eswinfo', views.lab7eswinfo, name='lab7eswinfo'),
     
 
     
--- LaboratoryDigitalization/Home/views.py
+++ LaboratoryDigitalization/Home/views.py
@@ -17,13 +17,38 @@ def lab7Binfo(request):
     lab7b_pc_list = Lab7B.objects.all()
     return render(request, 'lab7b.html', {'lab7b_pc_list': lab7b_pc_list})
 
+def lab7Cinfo(request):
+    lab7c_pc_list = Lab7C.objects.all()
+    return render(request, 'lab7c.html', {'lab7c_pc_list': lab7c_pc_list})
+
+def lab7Dinfo(request):
+    lab7d_pc_list = Lab7D.objects.all()
+    return render(request, 'lab7d.html', {'lab7d_pc_list': lab7d_pc_list})
+
+def lab7Einfo(request):
+    lab7e_pc_list = Lab7E.objects.all()
+    return render(request, 'lab7e.html', {'lab7e_pc_list': lab7e_pc_list})
+
 def lab7aswinfo(request):
-    lab7aswinfo_list = Lab7A_SW_Inst.objects.all()
-    return render(request, 'lab7aswinfo.html', {'lab7aswinfo_list': lab7aswinfo_list})
+    lab7ainfo_list = Lab7A_SW_Inst.objects.all()
+    return render(request, 'lab7aswinfo.html', {'lab7ainfo_list': lab7ainfo_list})
 
 def lab7bswinfo(request):
-    lab7bswinfo_list = Lab7B_SW_Inst.objects.all()
-    return render(request, 'lab7bswinfo.html', {'lab7bswinfo_list': lab7bswinfo_list})
+    lab7binfo_list = Lab7B_SW_Inst.objects.all()
+    return render(request, 'lab7bswinfo.html', {'lab7binfo_list': lab7binfo_list})
+
+def lab7cswinfo(request):
+    lab7cinfo_list = Lab7C_SW_Inst.objects.all()
+    return render(request, 'lab7cswinfo.html', {'lab7cinfo_list': lab7cinfo_list})
+
+def lab7dswinfo(request):
+    lab7dinfo_list = Lab7D_SW_Inst.objects.all()
+    return render(request, 'lab7dswinfo.html', {'lab7dinfo_list': lab7dinfo_list})
+
+def lab7eswinfo(request):
+    lab7einfo_list = Lab7E_SW_Inst.objects.all()
+    return render(request, 'lab7eswinfo.html', {'lab7einfo_list': lab7einfo_list})
+
 
 def upload(request):
     if request.method == "POST":
