40ca0917c225274ace91ddf243cbb9cfb82d8448
AditeeKore <aditeembk@gmail.com>
2021-09-21 14:12:24 +0530

    created models for all labs

--- LaboratoryDigitalization/Home/models.py
+++ LaboratoryDigitalization/Home/models.py
@@ -16,6 +16,7 @@ class AllLabinfo(models.Model):
     #     ordering = ["cod"]
     #     verbose_name_plural = "uploads"
 
+#LAB 7
 class Lab7A(models.Model):
     ComputerName = models.CharField(max_length=20, unique=True)
     Processor = models.CharField(max_length=200)
@@ -26,7 +27,6 @@ class Lab7A(models.Model):
     SpecialPurpose = models.CharField(max_length=150)
     DateOfPurchase = models.CharField(max_length=30)
     WorkingStatus = models.CharField(max_length=30)
-
 
 class Lab7B(models.Model):
     ComputerName = models.CharField(max_length=20, unique=True)
@@ -89,6 +89,159 @@ class Lab7D_SW_Inst(models.Model):
     Version = models.CharField(max_length=20, null=True)
 
 class Lab7E_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+#LAB 5
+class Lab5A(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab5B(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab5C(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab5D(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab5E(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab5A_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab5B_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab5C_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab5D_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab5E_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+#LAB 11
+class Lab11A(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab11B(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class Lab11A_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class Lab11B_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+#LAB CC
+class LabCC1(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class LabCC2(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class LabCC3(models.Model):
+    ComputerName = models.CharField(max_length=20, unique=True)
+    Processor = models.CharField(max_length=200)
+    RAM = models.CharField(max_length=10)
+    HardDisk = models.CharField(max_length=10)
+    MotherBoard = models.CharField(max_length=20)
+    Basic_OS = models.CharField(max_length=50)
+    SpecialPurpose = models.CharField(max_length=150)
+    DateOfPurchase = models.CharField(max_length=30)
+    WorkingStatus = models.CharField(max_length=30)
+
+class LabCC1_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class LabCC2_SW_Inst(models.Model):
+    SoftwareInstalled = models.CharField(max_length=50)
+    Version = models.CharField(max_length=20, null=True)
+
+class LabCC3_SW_Inst(models.Model):
     SoftwareInstalled = models.CharField(max_length=50)
     Version = models.CharField(max_length=20, null=True)
 
